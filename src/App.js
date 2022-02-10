d1276ee1fc281d969c999545414fb7ff030eff11
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2022-02-10 18:23:52 +0100

    render

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 export default class App extends Component {
   state = {
-    data: null,
+    data: [],
     loading: false,
     error: null,
   };
@@ -25,84 +25,17 @@ export default class App extends Component {
 
   render() {
     const { data, loading, error } = this.state;
+    const elements = data.map((item) => (
+      <li>
+        <p>{item.number}</p>
+      </li>
+    ));
     return (
       <div>
         {error && <h1>Brak faktury za wykonaną usługę</h1>}
         {loading && <h1>Loading...</h1>}
-        {data && <div>{data[1].buyer_city} </div>}
+        <ul>{elements}</ul>
       </div>
     );
   }
 }
-
-//         (result) => {
-//           this.setState({
-//             isLoader: true,
-//             items: result
-//           });
-//         },
-//         (error) => {
-//           this.setState({
-//             isLoader: true,
-//             error,
-//           });
-//         }
-//       );
-//   }
-//   render() {
-//     const { error, isLoader, items } = this.state;
-//     if (error) {
-//       return <p>Error {error.message}</p>;
-//     } else if (!isLoader) {
-//       return <p>Loading...</p>;
-//     } else {
-//       return (
-//         <ul>
-//           {items.map((item) => (
-//             <li key={item.id}>
-//               {item}
-
-//             </li>
-//           ))}
-//         </ul>
-//       );
-//     }
-//   }
-// }
-
-// import axios from "axios";
-
-// const fetchArticlesWithQuery = (searchQuery) => {
-//    axios
-//     .get(
-//       `https://vaska171717.fakturownia.pl/invoices.json?period=all&api_token=XKQiL2BafpNsI7Qfgy7&${searchQuery}`
-//     )
-//     .then((response) => response.json())
-
-// };
-
-// export default {
-//   fetchArticlesWithQuery,
-// };
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import articlesApi from "./articlesApi";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       articlesApi()
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// const [faktury, setFaktury] = useState([]);
-
-// const data = articlesApi.fetchArticlesWithQuery("Faktury").then;
-
-// articlesApi.fetchArticlesWithQuery("Faktury");
-// return <div className="App"></div>;
