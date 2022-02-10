1d62835d6e011ba4698e86f4801a9633a5145b08
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2022-02-10 21:17:42 +0100

    Updates

---
+++ precache-manifest.12a94afb05e6aae5e37348d22a17e490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511853cfef0ca9deb0bd77a3db5b0f6f",
+    "url": "/wygenerowanie-faktur/index.html"
+  },
+  {
+    "revision": "f7e024a286fa1e27c19f",
+    "url": "/wygenerowanie-faktur/static/js/2.15d0fe18.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/wygenerowanie-faktur/static/js/2.15d0fe18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08fb927ea9eb42647a1",
+    "url": "/wygenerowanie-faktur/static/js/main.f98bd157.chunk.js"
+  },
+  {
+    "revision": "606472fdbbbad068bfa0",
+    "url": "/wygenerowanie-faktur/static/js/runtime-main.fb227f6a.js"
+  }
+]);
